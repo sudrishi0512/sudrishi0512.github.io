98525d71749a241faba491cdb99a796f5c66604e
sudrishi0512 <31887579+sudrishi0512@users.noreply.github.com>
2021-10-27 16:12:46 +0530

    initial

---
+++ imc.js
@@ -0,0 +1,45 @@
+var arr = [];
+while(arr.length < 7){
+    var r = Math.floor(Math.random() * 3000) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+
+function addLi(element, winner) {
+     // Create the list item:
+     var item = document.createElement('li');
+     item.style.fontSize = 30;
+
+     // Set its contents:
+     item.appendChild(document.createTextNode(winner));
+
+     // Add it to the list:
+     element.appendChild(item);
+}
+
+// Add the contents of json to #foo:
+
+function get_next_winner()
+{
+    if(arr.length == 0)
+    {
+        document.getElementById('next').style.visibility = 'hidden';
+        document.getElementById('lotter_number').style.visibility = 'hidden';
+        document.getElementById('lotter_number').style.display = 'none';
+        document.getElementById('congrats').style.visibility = 'visible';
+        document.getElementById('congrats').style.display = 'initial';
+        document.getElementById('winner_banner').style.visibility = 'visible';
+        document.getElementById('winner_list').style.visibility = 'visible';
+    }
+    else
+    {
+        applause.pause();
+        applause.currentTime = 0;
+        applause.play();
+        document.getElementById('lotter_number').style.visibility = 'visible';
+        winner=arr.pop();
+        setTimeout(function(){
+            $('.odometer').html(winner);
+        }, 100);
+        addLi(document.getElementById('winner_list'), winner.toString().padStart(4, '0'));
+    }
+}
